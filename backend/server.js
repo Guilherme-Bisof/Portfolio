eb9856a0656646e9c7952b07fe845409f927cb8e
Guilherme-Bisof <gui_bisof@hotmail.com>
2026-04-04 19:09:53 -0300

    refactor: Remover emoji da mensagem de log ao iniciar o servidor

--- backend/server.js
+++ backend/server.js
@@ -217,5 +217,5 @@ app.delete("/projects/:id", authenticateToken, async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta http://localhost:${PORT}`);
+  console.log(` Servidor rodando na porta http://localhost:${PORT}`);
 });
