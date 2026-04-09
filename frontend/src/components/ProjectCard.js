ff34c4e368c647dad23d60d5beceae3d32b61d46
Guilherme-Bisof <gui_bisof@hotmail.com>
2026-04-09 00:42:34 -0300

    Fix: resolvido problema do "type" não definido

--- frontend/src/components/ProjectCard.js
+++ frontend/src/components/ProjectCard.js
@@ -2,14 +2,14 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 export default function ProjectCard({ project, onClick }) {
-  const { title, description, imageUrl, repoUrl, deployInput,technologies,types } = project;
+  const { title, description, imageUrl, repoUrl, deployInput,technologies,types,type } = project;
 
   return (
     <div
       onClick={onClick}
       className="bg-gray-900/50 rounded-lg p-6 flex flex-col justify-between h-full border min-h-[300px] border-cyan-400/30 shadow-[0_0_10px_rgba(0,255,255,0.1)] cursor-pointer hover:border-cyan-400/80 hover:shadow-[0_0_15px_rgba(0,255,255,0.3)]  transition-all"
     >
-      <span className='text-base font-bold mb-1 text-cyan-200 '>{types}</span>
+      <span className='text-base font-bold mb-1 text-cyan-200 '>{type}</span>
       <h3 className="text-xl font-bold mb-2 text-cyan-400">{title}</h3>
       <p className="text-gray-300 grow mb-4 line-clamp-4">{description}</p>
       <div className='flex flex-wrap gap-2'>
