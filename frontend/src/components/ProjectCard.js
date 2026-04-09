03d58ca7361d5a62ab754cfe6a4d1a490c532b98
Guilherme-Bisof <gui_bisof@hotmail.com>
2026-04-09 00:33:56 -0300

    Feat: adicionado novo sistema de tags de tipos de projetos

--- frontend/src/components/ProjectCard.js
+++ frontend/src/components/ProjectCard.js
@@ -7,8 +7,9 @@ export default function ProjectCard({ project, onClick }) {
   return (
     <div
       onClick={onClick}
-      className="bg-gray-900/50 rounded-lg p-6 flex flex-col h-full border border-cyan-400/30 shadow-[0_0_10px_rgba(0,255,255,0.1)] cursor-pointer hover:border-cyan-400/80 hover:shadow-[0_0_15px_rgba(0,255,255,0.3)]  hover:scale-[1.02]  transition-all"
+      className="bg-gray-900/50 rounded-lg p-6 flex flex-col h-full border border-cyan-400/30 shadow-[0_0_10px_rgba(0,255,255,0.1)] cursor-pointer hover:border-cyan-400/80 hover:shadow-[0_0_15px_rgba(0,255,255,0.3)]  transition-all size-3"
     >
+      <span className='text-base font-bold mb-1 text-cyan-200 '>{types}</span>
       <h3 className="text-xl font-bold mb-2 text-cyan-400">{title}</h3>
       <p className="text-gray-300 grow mb-4 line-clamp-4">{description}</p>
       <div className='flex flex-wrap gap-2'>
